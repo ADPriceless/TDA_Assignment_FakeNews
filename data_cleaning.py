6f10962f3d2655950fee1c433a1f0c470584000c
ADPriceless <47830638+ADPriceless@users.noreply.github.com>
2024-02-28 23:10:47 +0000

    more data cleaning: remove newline characters, combine title and text and split into test and train sets

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,12 @@
+# pylint: disable=missing-function-docstring,redefined-outer-name
+
+
 """A file to clean the news article dataset to be ready to
 preprocess for a model"""
 
+
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def clean_data(file_path: str) -> pd.DataFrame:
@@ -11,6 +16,9 @@ def clean_data(file_path: str) -> pd.DataFrame:
     df = _drop_rows(df, 'language', 'german')
     df = _keep_columns(df, ['title', 'text', 'label'])
     df = _drop_null(df)
+    df = _remove_newline_characters(df, 'title')
+    df = _remove_newline_characters(df, 'text')
+    df = _combine_columns(df, ['title', 'text'], 'text')
     df = _map_rename_label(df, 'label', 'is_fake', {'Real': 0, 'Fake': 1})
     return df
 
@@ -31,6 +39,19 @@ def _drop_null(df: pd.DataFrame) -> pd.DataFrame:
     return df.dropna()
 
 
+def _remove_newline_characters(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    removed = df[column].str.replace('\n','')
+    df[column] = removed
+    return df
+
+
+def _combine_columns(df: pd.DataFrame, columns: list[str], new_column: str) -> pd.DataFrame:
+    df[new_column] = df[columns[0]] + ' ' + df[columns[1]]
+    if new_column in columns:
+        columns.remove(new_column)
+    return df.drop(columns=columns)
+
+
 def _map_rename_label(
     df: pd.DataFrame,
     old_label: str,
@@ -43,8 +64,24 @@ def _map_rename_label(
     return df.rename(columns={old_label: new_label})
 
 
+def split_into_test_and_train_datasets(df: pd.DataFrame) -> tuple:
+    # split into train and test
+    df_train, df_test = train_test_split(
+        df, test_size=0.2, stratify=df['is_fake']
+    )
+    return df_train, df_test
+
+
+def save_dataset(df: pd.DataFrame, file_path: str) -> None:
+    df.to_csv(file_path, index=False)
+
+
 if __name__ == '__main__':
     # clean the dataset
     df_clean = clean_data('data/news_articles (2).csv')
-    # save the clean dataset
-    df_clean.to_csv('data/news_articles_clean.csv', index=False)
+    # split into train and test
+    df_train, df_test = split_into_test_and_train_datasets(df_clean)
+    # save the clean, train and test datasets
+    save_dataset(df_clean, 'data/news_articles_clean.csv')
+    save_dataset(df_train, 'data/train.csv')
+    save_dataset(df_test, 'data/test.csv')
